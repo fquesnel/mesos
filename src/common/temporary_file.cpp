15ed0a78e45498d46f04081e720528e4e73fd80c
Thomas Langé <t.lange@criteo.com>
2020-01-03 09:53:40 +0100

    Improved error catching in Mesos::Criteo::TemporaryFile class

--- src/common/temporary_file.cpp
+++ src/common/temporary_file.cpp
@@ -1,4 +1,5 @@
 #include "common/temporary_file.hpp"
+#include <cerrno>
 
 namespace mesos {
 namespace criteo {
@@ -11,7 +12,7 @@ TemporaryFile::TemporaryFile() {
   int fd = mkstemp(filepath);
   if (fd == -1)
     throw std::runtime_error(
-        "Unable to create temporary file to run commands");
+      "Unable to create temporary file to run commands : " + std::strerror(errno));
   close(fd);
   m_filepath = std::string(filepath);
 }
@@ -21,23 +22,35 @@ TemporaryFile::TemporaryFile() {
  * @return The content of the file.
  */
 std::string TemporaryFile::readAll() const {
-  std::ifstream ifs(m_filepath);
-  std::string content((std::istreambuf_iterator<char>(ifs)),
-                      (std::istreambuf_iterator<char>()));
-  ifs.close();
+  std::ifstream ifs;
+  ifs.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+  try {
+    ifs.open(m_filepath);
+    std::string content((std::istreambuf_iterator<char>(ifs)),
+                        (std::istreambuf_iterator<char>()));
+    ifs.close();
+  }
+  catch(std::ifstream::failure &e) {
+    throw std::runtime_error("Exception while accessing temporary file : " + std::strerror(errno));
+  }
   return content;
 }
 
 /*
- * Write content to the temporary file and flush it.
+ * Write content to the temporary file.
  * @param content The content to write to the file.
  */
 void TemporaryFile::write(const std::string& content) const {
   std::ofstream ofs;
-  ofs.open(m_filepath);
-  ofs << content;
-  std::flush(ofs);
-  ofs.close();
+  ofs.exceptions(std::ofstream::failbit | std::ofstream::badbit);
+  try {
+    ofs.open(m_filepath);
+    ofs << content;
+    ofs.close();
+  }
+  catch(std::ofstream::failure &e) {
+    throw std::runtime_error("Exception while accessing temporary file : " + std::strerror(errno));
+  }
 }
 
 friend std::ostream& TemporaryFile::operator<<(std::ostream& out, const TemporaryFile& temp_file) {
